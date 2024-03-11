16a00eadb052a1a29f7ad4c2664d0322954cb435
AJ <ajblankenship1@gmail.com>
2024-03-11 07:29:27 +0000

    added glossary drop down with symbol definitions

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 console.log("loaded");
 
 document.querySelector("#symbols").addEventListener("click",(event) => {
-    console.log("ive been clicked");
+   
     const element = document.querySelector(".definitions");
-    
-    console.log(element.style.maxHeight);
-    
+
     if (element.style.maxHeight){
         element.style.maxHeight = "";
+        document.querySelector(".expander").style.transform = "rotate(180deg)";
     }
     else{
         element.style.maxHeight= "2000px";
+        document.querySelector(".expander").style.transform = "rotate(0deg)";
     }
 })
